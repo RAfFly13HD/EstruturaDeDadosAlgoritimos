017b4a750450029031ea8501db5909b032d68cdb
RAfFly13HD <130411995+RAfFly13HD@users.noreply.github.com>
2024-09-07 01:10:07 +0000

    a

---
+++ EstruturaDeDadosAlgoritimos/aula06/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+    public static void main(String[] args) {
+        ListaDupla<String> lista = new ListaDupla<String>("Alunos");
+
+        lista.imprimeLista();
+        lista.addInicio("Rafael");
+        lista.imprimeLista();
+        lista.addInicio("Miguel");
+        lista.imprimeLista();
+        lista.addFinal("Gabriel");
+        lista.imprimeLista();
+        lista.addFinal("Daniel");
+        lista.imprimeLista();
+
+        // lista.removeInicio();
+        lista.imprimeLista();
+        lista.removerFinal();
+        // lista.removeFinal();
+        lista.imprimeLista();
+    }
+}
